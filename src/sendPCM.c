00f8549be7663da66303ffd87f10f2aeb7847878
rayveil <rayveil@aliyun.com>
2021-07-14 00:06:53 +0800

    fix rtp send func
    Signed-off-by: rayveil <rayveil@aliyun.com>

--- src/sendPCM.c
+++ src/sendPCM.c
@@ -11,14 +11,12 @@
 #include "g711codec.h"
 
 //网络参数,有些端口vlc是不支持的
-#define RTP_IP      "127.0.0.1"
+#define RTP_IP      "192.168.3.32"
 #define RTP_PORT    9832
 
 //发送文件参数
 #define SEND_CHN    1
 #define SEND_FREQ   8000
-#define SEND_FILE   "./8000.wav"
-
 //每包数据发出后的延时
 #define SEND_DELAYUS   20000
 
@@ -37,30 +35,39 @@ int main(int argc, char* argv[])
     RtpPacket rtpPacket;
     int fd;
     int seekStart = 0;
-    //
+    char audiofile[64] = {"./8000.wav"};
+    int chn = SEND_CHN;
     unsigned char pcm[SEND_TIMESTAMP*2];
+    int delaytime = 0;
 
-    if(argc != 2)
+    if(argc < 2)
     {
-        printf("Usage: %s <pkg type: 0/pcma 1/pcmu>\n", argv[0]);
+        printf("Usage: %s <pkg type: 0/pcma 1/pcmu> <filename: default %s> <ch: default %d>\n", argv[0],audiofile,SEND_CHN);
         return -1;
     }
-
     sscanf(argv[1], "%d", &pkgType);
     if(pkgType == 0)
         pkgType = RTP_PAYLOAD_TYPE_PCMA;
     else
         pkgType = RTP_PAYLOAD_TYPE_PCMU;
 
-    fd = open(SEND_FILE, O_RDONLY);
+    if(argc >= 3){
+        memset(audiofile,0,64);
+        sscanf(argv[2], "%s", &audiofile);
+    }
+   if(argc >= 4)
+        sscanf(argv[3], "%d", &chn);
+    delaytime = SEND_DELAYUS/chn;
+
+    fd = open(audiofile, O_RDONLY);
     if(fd < 0)
     {
-        printf("failed to open %s\n", SEND_FILE);
+        printf("failed to open %s\n", audiofile);
         return -1;
     }
 
     //wav格式跳过文件头
-    if(strstr(SEND_FILE, ".wav"))
+    if(strstr(audiofile, ".wav"))
     {
         seekStart = 44;
         read(fd, rtpPacket.payload, seekStart);
@@ -88,7 +95,7 @@ int main(int argc, char* argv[])
         if(!wsdp)
         {
             wsdp = 1;
-            rtp_create_sdp("./test.sdp", RTP_IP, RTP_PORT, SEND_CHN, SEND_FREQ, pkgType);
+            rtp_create_sdp("./test.sdp", RTP_IP, RTP_PORT, chn, SEND_FREQ, pkgType);
         }
 
         //读文件
@@ -115,8 +122,8 @@ int main(int argc, char* argv[])
 
         //保证每次发包延时一致
         tick2 = getTickUs();
-        if(tick2 > tick1 && tick2 - tick1 < SEND_DELAYUS)
-            usleep(SEND_DELAYUS - (tick2 - tick1));
+        if(tick2 > tick1 && tick2 - tick1 < delaytime)
+            usleep(delaytime - (tick2 - tick1));
         else
             usleep(1000);
         // printf("tick1: %d, tick2: %d, delay: %d\n", tick1, tick2, SEND_DELAYUS-(tick2-tick1));
